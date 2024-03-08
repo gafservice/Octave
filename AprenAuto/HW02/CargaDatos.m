160144f4ce6476e5a0ef73a3620385ce491958ae
gafservice <39964081+gafservice@users.noreply.github.com>
2024-03-08 08:43:28 -0600

    Update CargaDatos.m

--- AprenAuto/HW02/CargaDatos.m
+++ AprenAuto/HW02/CargaDatos.m
@@ -6,17 +6,16 @@ pkg load image;
 pkg load io;
 
 % Especifica la ruta del archivo CSV
-ubi_file = 'BPC.csv';
+ubi_file = '/home/famltp02/Documents/GitHub/Octave/AprenAuto/HW02/BPC.csv';
 
-
+% Usa csv2cell para cargar los datos en una celda
+pkg load io;
 ## datos_celda = csv2cell(ubi_file);
 datos = csvread(ubi_file);
-md = csvread(ubi_file);
 ##Muestra los datos en forma de celda
 ##disp(datos_celda);
 ## z = csvread(ubi_file, 1,1, 1,6)
-##y = dlmread (ubi_file, 2, 2, 2:5)
-x = md(2:11, 3)
+y = dlmread (ubi_file, 2, 2, 2:5)
 ## z = datos_celda(2:11, 3)
 ## X = datos_celda(2:11, 4:5)
 ## p = [150, 3; 200, 4; 120, 2];
