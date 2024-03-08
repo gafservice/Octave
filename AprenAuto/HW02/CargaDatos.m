701cfd1d096ea470f886c7b20e023c97cb5657da
gafservice <39964081+gafservice@users.noreply.github.com>
2024-03-08 07:43:15 -0600

    cargar datos

--- AprenAuto/HW02/CargaDatos.m
+++ AprenAuto/HW02/CargaDatos.m
@@ -5,8 +5,6 @@ pkg load image;
 ## sudo apt-get install octave-io
 pkg load io;
 
-
-
 % Especifica la ruta del archivo CSV
 ubi_file = '/home/famltp02/Documents/GitHub/Octave/AprenAuto/HW02/BPC.csv';
 
@@ -16,10 +14,8 @@ pkg load io;
 datos = csvread(ubi_file);
 ##Muestra los datos en forma de celda
 ##disp(datos_celda);
-
 ## z = csvread(ubi_file, 1,1, 1,6)
 y = dlmread (ubi_file, 2, 2, 2:5)
-
 ## z = datos_celda(2:11, 3)
 ## X = datos_celda(2:11, 4:5)
 ## p = [150, 3; 200, 4; 120, 2];
